5d5dc85cc4d152b5859468de41f61afa8ef6a42d
Dênnis Dantas <ddspog@gmail.com>
2016-04-04 20:48:34 -0300

    Add views on project
    The views package will contain, all scala.html files, which render
    document html when someone call index via GET request.
    
    That way we can alter data loaded on the website, basing on each user
    logged, or the ads created.
    
     - Add views on project, as a test.

--- app/Global.java
+++ app/Global.java
@@ -6,12 +6,18 @@ import java.time.LocalDateTime;
 
 public class Global extends GlobalSettings {
 
+	/**
+	 * Executa operações antes do server da aplicação ligar.
+	 */
 	@Override
 	public void onStart(Application app) {
 		Logger.info(">>> Aplicacao wgmt inicializada...");
 		Logger.info(">>> Hora = " + LocalDateTime.now());
 	}
 
+	/**
+	 * Executa operações após o server da aplicação desligar.
+	 */
     @Override
 	public void onStop(Application app) {
         Logger.info(">>> Aplicacao wgmt finalizando...");
