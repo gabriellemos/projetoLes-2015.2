b8e2d2c34527544fbe50a2b739dd838fcf790cce
Gabriel Lemos <Gabriel Lemos>
2016-04-05 22:49:25 -0300

    Adicionado modelo vazio de Confeiteiro para possibilitar criacao dos testes

---
+++ app/models/Confeiteiro.java
@@ -0,0 +1,51 @@
+package models;
+
+import java.util.HashSet;
+
+public class Confeiteiro {
+
+    public Confeiteiro() {}
+    public Confeiteiro(String nome) {}
+
+    public Object getNome() {
+        return null;
+    }
+
+    public Object getEndereco() {
+        return null;
+    }
+
+    public Object getEmail() {
+        return null;
+    }
+
+    public Object getContato() {
+        return null;
+    }
+
+    public Object getId() {
+        return null;
+    }
+
+    public HashSet<Object> getAnuncios() {
+        return null;
+    }
+
+    public void addAnuncio(Anuncio anuncio) {
+    }
+
+    public void setNome(String nome) {
+    }
+
+    public void setEndereco(String endereco) {
+    }
+
+    public void setEmail(String email) {
+    }
+
+    public void setContato(String contato) {
+    }
+
+    public void setId(String id) {
+    }
+}
