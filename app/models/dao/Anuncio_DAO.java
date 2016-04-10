db796ca4ccf4bd7a25d9075efdb768e3ebeb5d56
Gabriel Lemos <Gabriel Lemos>
2016-04-09 22:23:09 -0300

    Integração do create Anuncio com o BD

---
+++ app/models/dao/Anuncio_DAO.java
@@ -0,0 +1,48 @@
+package models.dao;
+
+import models.Anuncio;
+import models.Confeiteiro;
+import models.TipoAnuncio;
+import models.Utils;
+import org.apache.poi.openxml4j.exceptions.InvalidOperationException;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by gabriel on 08/04/2016.
+ */
+public class Anuncio_DAO {
+
+    private static java.sql.Connection conexao= null;
+    private static Statement declaracao = null;
+    private static String strSql;
+
+    /*
+     * Cria um Anúncio no banco de dados. Um id é atribuido automaticamente no ato da criação.
+     */
+    public static void createAnuncio (String titulo, String descricao, String dataEdicao,
+                               String dataCriacao, String preco, String criador, String tipo)
+            throws Exception {
+
+        try {
+            conexao= Connection.getConnection();
+            declaracao= conexao.createStatement();
+
+            strSql = "INSERT INTO Anuncio (titulo_anuncio, descricao_anuncio, edicao_anuncio, " +
+                    "criacao_anuncio, preco_anuncio, criador_anuncio, tipo_anuncio) VALUES('" +
+                    titulo + "','" + descricao + "','" + dataEdicao + "','" + dataCriacao + "','" +
+                    preco + "','" + criador  + "','" + tipo + "')";
+
+            declaracao.executeLargeUpdate(strSql);
+            declaracao.close();
+            conexao.commit();
+            conexao.close();
+        } catch(Exception e){
+            throw new InvalidOperationException("Erro ao criar o Anúncio");
+        }
+    }
+
+
+}
