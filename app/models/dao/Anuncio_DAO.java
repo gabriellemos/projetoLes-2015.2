49d9ddfebaefb0c8700b00a5ead39e8f737c6c63
Gabriel Lemos <Gabriel Lemos>
2016-04-09 22:24:26 -0300

    Integracao do get Anuncio com base num ID

--- app/models/dao/Anuncio_DAO.java
+++ app/models/dao/Anuncio_DAO.java
@@ -63,5 +63,41 @@ public class Anuncio_DAO {
         }
     }
 
-
+    /*
+     * Retorna um objeto Anúncio de acordo com o ID no banco de dados.
+     */
+    public static Anuncio getAnuncio(int ID) throws Exception{
+        try {
+            ResultSet resultado;
+            Anuncio resposta= new Anuncio();
+            conexao = Connection.getConnection();
+            declaracao = conexao.createStatement();
+            strSql = "SELECT * FROM Anuncio a WHERE a.id_anuncio='" + ID + "';";
+            resultado = declaracao.executeQuery(strSql);
+            // TODO: Verifica uma forma mais simples de resolver isso, utilizando o contutor da classe
+            resposta.setId(resultado.getInt("id_anuncio"));
+            resposta.setTitulo(resultado.getString("titulo_anuncio"));
+            resposta.setDescricao(resultado.getString("descricao_anuncio"));
+            resposta.setDataEdicao(Utils.converteDateToGregorianCalendar(
+                    resultado.getDate("edicao_anuncio")
+            ));
+            resposta.setDataCriacao(Utils.converteDateToGregorianCalendar(
+                    resultado.getDate("criacao_anuncio")
+            ));
+            resposta.setPreco(resultado.getFloat("preco_anuncio"));
+            resposta.setCriador(Confeiteiro_DAO.GetConfeiteiro(
+                    resultado.getInt("criador_anuncio")
+            ));
+            resposta.setTipoAnuncio(TipoAnuncio.valueOf(
+                    resultado.getString("tipo-anuncio")
+            ));
+            //ate aqui
+            declaracao.close();
+            conexao.close();
+            return resposta;
+            // Verifica a exceção do bd
+        }catch (Exception e){
+            throw new InvalidOperationException("Confeiteiro Inexistente");
+        }
+    }
 }
