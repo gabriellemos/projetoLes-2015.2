d4cf0c0229e6bdde008020b4b5543aeea6ca5fe5
BoloBolo <gbajordan@gmail.com>
2016-04-11 15:36:19 -0300

    Criacao da tabela de Anuncio no BD

---
+++ app/models/dao/Anuncio_Tabela.java
@@ -0,0 +1,43 @@
+package models.dao;
+
+import java.sql.Statement;
+
+/**
+ * Created by jordan on 08/04/2016.
+ */
+
+
+public class Anuncio_Tabela {
+    private static java.sql.Connection conexao= null;
+    private static Statement declaracao = null;
+    private static  String str;
+
+    //NAO EXECUTE ESSA CLASSE OU VAI GERA UM ERRO DIZENDO QUE A TABELA JA FOI CRIADA
+    public static void main( String args[] ) throws Exception {
+
+        try{
+            conexao= models.dao.Connection.getConnection();
+            declaracao= conexao.createStatement();
+
+            str = "CREATE TABLE Anuncio " +
+                    "(ID_Anuncio SERIAL PRIMARY KEY," +
+                    "Titulo_Anuncio         CHAR(32)    NOT NULL, " +
+                    "Descricao_Anuncio      CHAR(256), " +
+                    "Edicao_Anuncio         DATE, " +
+                    "Criacao_Anuncio        DATE NOT NULL,"  +
+                    "Preco_Anuncio          MONEY DEFAULT 0.00 NOT NULL,"  + // Default 0.0
+                    "Criador_Anuncio        INT references Confeiteiro(ID_Confeiteiro),"  +  // Chave estrangeira
+                    "Tipo_Anuncio           CHAR(16) NOT NULL )";
+
+
+            declaracao.executeUpdate(str);
+            declaracao.close();
+            conexao.close();
+            System.out.print("Sucesso");}
+        catch (Exception e){
+            System.out.print(e.getMessage());
+
+        }
+
+    }
+}
