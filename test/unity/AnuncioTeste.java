99174486f21e79da9d8a30eb6ab78d038c711a5a
alineatc <aline.costa.cc@gmail.com>
2016-04-13 22:18:21 -0300

    Finaliza testes de unidade de Anuncio

--- test/unity/AnuncioTeste.java
+++ test/unity/AnuncioTeste.java
@@ -18,25 +18,28 @@ import java.util.GregorianCalendar;
  */
 public class AnuncioTeste {
 
-
     protected Anuncio anuncioDefault2;
     protected Anuncio anuncioComum;
     protected Anuncio anuncioAniversario;
     protected Anuncio anuncioCasamento;
 
-    protected Confeiteiro confeteiroDefault;
+    protected Confeiteiro confeiteiroDefault;
     protected Confeiteiro confeiteiroSemAnuncios;
 
     @Before
     public void setUp() {
-        confeteiroDefault = new Confeiteiro();
+        confeiteiroDefault = new Confeiteiro();
         confeiteiroSemAnuncios = new Confeiteiro("Bolo");
 
         anuncioDefault2 = new Anuncio();
 
-        anuncioComum = new Anuncio("Este é um anuncio comum", confeteiroDefault);
-        anuncioAniversario = new Anuncio("Este é um anuncio de aniversario", confeiteiroSemAnuncios, "Anuncio de aniversario para testes", TipoAnuncio.ANIVERSARIO, 5);
-        anuncioCasamento = new Anuncio("Este é um anuncio de casamento", confeiteiroSemAnuncios, "Anuncio de casamento para testes", TipoAnuncio.CASAMENTO, 50);
+        anuncioComum = new Anuncio("Este é um anuncio comum", confeiteiroDefault);
+        anuncioAniversario = new Anuncio("Este é um anuncio de aniversario",
+                confeiteiroSemAnuncios, "Anuncio de aniversario para testes",
+                TipoAnuncio.ANIVERSARIO, 5);
+        anuncioCasamento = new Anuncio("Este é um anuncio de casamento",
+                confeiteiroSemAnuncios, "Anuncio de casamento para testes",
+                TipoAnuncio.CASAMENTO, 50);
 
     }
 
@@ -89,28 +92,35 @@ public class AnuncioTeste {
     public void TestaGetSetTitulo() {
         Assert.assertNull(anuncioDefault2.getTitulo());
         anuncioDefault2.setTitulo("Novo titulo para anuncio default 2");
-        Assert.assertEquals(anuncioDefault2.getTitulo(), "Novo titulo para anuncio default 2");
+        Assert.assertEquals(anuncioDefault2.getTitulo(),
+                "Novo titulo para anuncio default 2");
 
         Assert.assertEquals(anuncioComum.getTitulo(), "Este é um anuncio comum");
         anuncioComum.setTitulo("Novo titulo para anuncio comum");
-        Assert.assertEquals(anuncioComum.getTitulo(), "Novo titulo para anuncio comum");
+        Assert.assertEquals(anuncioComum.getTitulo(),
+                "Novo titulo para anuncio comum");
 
-        Assert.assertEquals(anuncioCasamento.getTitulo(), "Este é um anuncio de casamento");
+        Assert.assertEquals(anuncioCasamento.getTitulo(),
+                "Este é um anuncio de casamento");
         anuncioCasamento.setTitulo("Novo titulo para anuncio casamento");
-        Assert.assertEquals(anuncioCasamento.getTitulo(), "Novo titulo para anuncio casamento");
+        Assert.assertEquals(anuncioCasamento.getTitulo(),
+                "Novo titulo para anuncio casamento");
     }
 
     @Test
     public void TestaSetTituloInvalido() {
-        String[] titulosInvalidos = {null, "", "     "};
+        String[] titulosInvalidos = { null, "", "     " };
 
         for (String tituloInvalido : titulosInvalidos) {
             try {
                 anuncioAniversario.setTitulo(tituloInvalido);
-                Assert.fail("Foi possível setar o titulo do anuncio para: " + tituloInvalido);
+                Assert.fail("Foi possível setar o titulo do anuncio para: "
+                        + tituloInvalido);
             } catch (IllegalArgumentException exception) {
-                Assert.assertEquals(IllegalArgumentException.class, exception.getClass());
-                Assert.assertEquals(exception.getMessage(), "Argumento 'Título' recebendo valores inválidos");
+                Assert.assertEquals(IllegalArgumentException.class,
+                        exception.getClass());
+                Assert.assertEquals(exception.getMessage(),
+                        "Argumento 'Título' recebendo valores inválidos");
             }
         }
     }
@@ -119,28 +129,35 @@ public class AnuncioTeste {
     public void TestaGetSetDescricao() {
         Assert.assertNull(anuncioDefault2.getDescricao());
         anuncioDefault2.setDescricao("Nova descricao para anuncio default 2");
-        Assert.assertEquals(anuncioDefault2.getDescricao(), "Nova descricao para anuncio default 2");
+        Assert.assertEquals(anuncioDefault2.getDescricao(),
+                "Nova descricao para anuncio default 2");
 
         Assert.assertEquals(anuncioComum.getDescricao(), "Sem descrição");
         anuncioComum.setDescricao("Adiciona descricao para anuncio comum");
-        Assert.assertEquals(anuncioComum.getDescricao(), "Adiciona descricao para anuncio comum");
+        Assert.assertEquals(anuncioComum.getDescricao(),
+                "Adiciona descricao para anuncio comum");
 
-        Assert.assertEquals(anuncioCasamento.getDescricao(), "Anuncio de casamento para testes");
+        Assert.assertEquals(anuncioCasamento.getDescricao(),
+                "Anuncio de casamento para testes");
         anuncioCasamento.setDescricao("Nova descricao para anuncio casamento");
-        Assert.assertEquals(anuncioCasamento.getDescricao(), "Nova descricao para anuncio casamento");
+        Assert.assertEquals(anuncioCasamento.getDescricao(),
+                "Nova descricao para anuncio casamento");
     }
 
     @Test
     public void TestaSetDescricaoInvalida() {
-        String[] descricoesInvalidas = {null, "", "     "};
+        String[] descricoesInvalidas = { null, "", "     " };
 
         for (String descricaoInvalida : descricoesInvalidas) {
             try {
                 anuncioAniversario.setDescricao(descricaoInvalida);
-                Assert.fail("Foi possível setar a descricao do anuncio para: " + descricaoInvalida);
+                Assert.fail("Foi possível setar a descricao do anuncio para: "
+                        + descricaoInvalida);
             } catch (IllegalArgumentException exception) {
-                Assert.assertEquals(IllegalArgumentException.class, exception.getClass());
-                Assert.assertEquals(exception.getMessage(), "Argumento 'Descrição' recebendo valores inválidos");
+                Assert.assertEquals(IllegalArgumentException.class,
+                        exception.getClass());
+                Assert.assertEquals(exception.getMessage(),
+                        "Argumento 'Descrição' recebendo valores inválidos");
             }
         }
     }
@@ -159,5 +176,97 @@ public class AnuncioTeste {
         anuncioAniversario.setPreco(1000);
         Assert.assertTrue(anuncioAniversario.getPreco() == 1000);
     }
-    
+
+    @Test
+    public void TestaSetPrecoInvalida() {
+        float precoInvalido = -1;
+
+        try {
+            anuncioCasamento.setPreco(precoInvalido);
+            Assert.fail("Foi possível setar o preco do anuncio para: "
+                    + precoInvalido);
+        } catch (IllegalArgumentException exception) {
+            Assert.assertEquals(IllegalArgumentException.class,
+                    exception.getClass());
+            Assert.assertEquals(exception.getMessage(),
+                    "Argumento 'Preço' recebendo valores inválidos");
+        }
+
+        Assert.assertTrue(anuncioCasamento.getPreco() == 50.0);
+    }
+
+    @Test
+    public void TestaGetSetCriador() {
+        Assert.assertNull(anuncioDefault2.getCriador());
+        anuncioDefault2.setCriador(confeiteiroSemAnuncios);;
+        Assert.assertEquals(anuncioDefault2.getCriador(), confeiteiroSemAnuncios);
+        Assert.assertNotEquals(anuncioDefault2.getCriador(), confeiteiroDefault);
+
+        Assert.assertEquals(anuncioCasamento.getCriador(), confeiteiroSemAnuncios);
+        anuncioCasamento.setCriador(confeiteiroDefault);
+        Assert.assertEquals(anuncioCasamento.getCriador(), confeiteiroDefault);
+        Assert.assertNotEquals(anuncioCasamento.getCriador(), confeiteiroSemAnuncios);
+
+        confeiteiroSemAnuncios.addAnuncio(anuncioAniversario);
+        anuncioCasamento.setCriador(confeiteiroSemAnuncios);
+        Assert.assertTrue(anuncioCasamento.getCriador().getAnuncios().size() == 1);
+    }
+
+    @Test
+    public void TestaSetCriadorInvalida() {
+        Confeiteiro confeiteiroNull = null;
+
+        try {
+            anuncioCasamento.setCriador(confeiteiroNull);
+            Assert.fail("Foi possível setar o criador do anuncio para: "
+                    + confeiteiroNull);
+        } catch (IllegalArgumentException exception) {
+            Assert.assertEquals(IllegalArgumentException.class,
+                    exception.getClass());
+            Assert.assertEquals(exception.getMessage(),
+                    "Argumento 'Criador' recebendo valores inválidos");
+        }
+
+        Assert.assertEquals(anuncioCasamento.getCriador(), confeiteiroSemAnuncios);
+    }
+
+    @Test
+    public void TestaGetSetTipoAnuncio() {
+        Assert.assertEquals(anuncioComum.getTipoAnuncio(), TipoAnuncio.COMUM);
+        anuncioComum.setTipoAnuncio(TipoAnuncio.ANIVERSARIO);;
+        Assert.assertEquals(anuncioComum.getTipoAnuncio(), TipoAnuncio.ANIVERSARIO);
+        Assert.assertNotEquals(anuncioComum.getTipoAnuncio(), TipoAnuncio.COMUM);
+
+        Assert.assertEquals(anuncioAniversario.getTipoAnuncio(), TipoAnuncio.ANIVERSARIO);
+        anuncioAniversario.setTipoAnuncio(TipoAnuncio.CASAMENTO);
+        Assert.assertEquals(anuncioAniversario.getTipoAnuncio(), TipoAnuncio.CASAMENTO);
+        Assert.assertNotEquals(anuncioAniversario.getTipoAnuncio(), TipoAnuncio.ANIVERSARIO);
+        Assert.assertNotEquals(anuncioAniversario.getTipoAnuncio(), TipoAnuncio.COMUM);
+
+        Assert.assertEquals(anuncioCasamento.getTipoAnuncio(), TipoAnuncio.CASAMENTO);
+        anuncioCasamento.setTipoAnuncio(TipoAnuncio.COMUM);
+        Assert.assertEquals(anuncioCasamento.getTipoAnuncio(), TipoAnuncio.COMUM);
+        Assert.assertNotEquals(anuncioCasamento.getTipoAnuncio(), TipoAnuncio.ANIVERSARIO);
+        Assert.assertNotEquals(anuncioCasamento.getTipoAnuncio(), TipoAnuncio.CASAMENTO);
+
+    }
+
+    @Test
+    public void TestaSetTipoAnuncioInvalida() {
+        TipoAnuncio tipoAnuncioInvalido = null;
+
+        try {
+            anuncioAniversario.setTipoAnuncio(tipoAnuncioInvalido);
+            Assert.fail("Foi possível setar o tipo de anuncio do anuncio para: "
+                    + tipoAnuncioInvalido);
+        } catch (IllegalArgumentException exception) {
+            Assert.assertEquals(IllegalArgumentException.class,
+                    exception.getClass());
+            Assert.assertEquals(exception.getMessage(),
+                    "Argumento 'Tipo de Anúncio' recebendo valores inválidos");
+        }
+
+        Assert.assertEquals(anuncioAniversario.getTipoAnuncio(), TipoAnuncio.ANIVERSARIO);
+    }
+
 }
