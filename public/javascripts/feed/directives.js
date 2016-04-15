95ab9049da89808af6f816ec31e7430344f59a11
Dênnis Dantas <ddspog@gmail.com>
2016-04-15 18:18:45 -0300

    Mod the directives to follow MDL
    I've modify all directives to follow the MDL layout that I've created
    for the feed. The new directives are the tool-bar, whom will show title
    of current section and the user btns. Also, the ad-modal will be
    responsible of showing all contacts informations of the ad selected.
    
     - Add new directives: tool-bar and ad-modal
     - Mod all other directives to use MDL

--- public/javascripts/feed/directives.js
+++ public/javascripts/feed/directives.js
@@ -1,53 +1,80 @@
-/* Directive declaring a navbar, linking to other sections of page, brand  */
+/* Directive declaring a navbar, linking to other sections of page, brand */
 feedApp.directive('navBar', function() {
     return {
-        restrict: 'E',
+        restrict: 'A',
         scope: {
             item: '=item'
         },
-        controller: ['$scope', '$parse', "$http", "$window", function($scope, $parse, $http, $window) {
+        templateUrl: '/assets/html/feed/directives/nav-bar.html'
+    };
+});
+
+/* Directive informing section, and with button indicating login */
+feedApp.directive('toolBar', function() {
+    return {
+        restrict: 'A',
+        scope: {
+            item: '=item',
+            title: '=title'
+        },
+        controller: ['$parse', "$http", "$window", "$state", function($scope, $parse, $http, $window, $state) {
             $scope.callFunc = function(exp) {
-                $parse(exp)($scope); //Parse the function name to get the expression and invoke it on the scope
+                //Parse the function name to get the expression and invoke it on the scope
+                $parse(exp)($scope);
             };
             $scope.logout = function() {
+                // Execute logout route, and refresh page
                 $http.get('/logout').success(function() {
                     $window.location.reload();
                 }).error(function() {});
             };
+            $scope.getStateName = function(){
+                // Get the current state title
+                try {
+                    $scope.currentState = $state.get($state.current).data.title;
+                }
+                catch(err) {
+                    $scope.currentState =  '';
+                }
+            };
         }],
-        templateUrl: '/assets/html/feed/directives/navbar.html'
+        templateUrl: '/assets/html/feed/directives/tool-bar.html'
     };
 });
 
 /* Directive declaring a footer bar, linking to other sections of page, and credits */
 feedApp.directive('footBar', function() {
     return {
-        restrict: 'E',
+        restrict: 'A',
         scope: {
             item: '=item'
         },
-        templateUrl: '/assets/html/feed/directives/footbar.html'
+        templateUrl: '/assets/html/feed/directives/foot-bar.html'
     };
 });
 
-/* Directive declaring a Ad card, showing informations of a cake announced */
-feedApp.directive('titleJumbo', function() {
-    return {
-        restrict: 'E',
-        scope: {
-            item: '=item'
-        },
-        templateUrl: '/assets/html/feed/directives/titlejumbo.html'
-    };
-});
-
-/* Directive declaring a Ad card, showing informations of a cake announced */
+/* Directive declaring an Ad card, showing information of a cake announced */
 feedApp.directive('adCard', function() {
     return {
-        restrict: 'E',
+        restrict: 'A',
         scope: {
-            item: '=item'
+            item: '=item',
+            labels: '=labels',
+            index: '=index'
         },
-        templateUrl: '/assets/html/feed/directives/adcard.html'
+        templateUrl: '/assets/html/feed/directives/ad-card.html'
+    };
+});
+
+/* Directive declaring a Ad Modal, showing more information of a cake announced */
+feedApp.directive('adModal', function() {
+    return {
+        restrict: 'A',
+        scope: {
+            item: '=item',
+            labels: '=labels',
+            index: '=index'
+        },
+        templateUrl: '/assets/html/feed/directives/ad-modal.html'
     };
 });
