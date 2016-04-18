5f0940028b8245adce2089d0d673c270dc171934
BoloBolo <gbajordan@gmail.com>
2016-04-18 17:43:51 -0300

    Criação do model de endereço

---
+++ app/models/dao/Endereco.java
@@ -0,0 +1,75 @@
+package models.dao;
+
+/**
+ * Created by jordan on 18/04/2016.
+ */
+public class Endereco {
+
+    private String rua;
+    private int id;
+    private String cidade;
+    private String numero;
+    private String bairro;
+    private String cep;
+    private String estado;
+
+    public  Endereco(){}
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+
+}
