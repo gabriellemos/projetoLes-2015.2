50f771ceaafb384651d4ee448447add338449089
Gabriel Lemos <Gabriel Lemos>
2016-04-26 15:19:18 -0300

    Corrigido movimentacao de modelos

---
+++ app/models/Contato.java
@@ -0,0 +1,7 @@
+package models;
+
+/**
+ * Created by Gabriel on 26/04/2016.
+ */
+public class Contato {
+}
---
+++ app/models/dao/UtilsBD.java
@@ -0,0 +1,7 @@
+package models.dao;
+
+/**
+ * Created by Gabriel on 25/04/2016.
+ */
+public class UtilsBD {
+}
